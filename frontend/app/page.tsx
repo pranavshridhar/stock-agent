05eebf7045f1dc25702cf8b1fe7dbc94a21d3abf
jared2183 <jaredmyang@gmail.com>
2025-04-05 13:52:55 -0500

    added general page layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Tickerbar from "./components/Tickerbar";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl">Hello</h1>
+    <div className="flex flex-row items-center justify-between min-h-screen p-24 border">
+      <Tickerbar/>
+      <div className="flex flex-col h-full w-full">
+        <div className="flex items-center justify-center p-5 border">
+          news header
+        </div>
+        <div className="flex items-center justify-center border">
+          chatbot box
+        </div>
+      </div>
     </div>
   );
 }
